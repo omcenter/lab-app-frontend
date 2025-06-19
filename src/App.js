2f3c8112df2a146094c716e05c8772d259c3062a
omcenter <omdiagnosticcenterdelhi@gmail.com>
2025-06-19 17:05:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
 import DoctorForm from './pages/DoctorForm';
 import AgentForm from './pages/AgentForm';
