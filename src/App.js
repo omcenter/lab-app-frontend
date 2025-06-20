e632970f49abe4636106aa1e245feb51a4432262
omcenter <omdiagnosticcenterdelhi@gmail.com>
2025-06-20 10:41:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import AdminDashboard from './pages/AdminDashboard';
+import SalesmanDashboard from './pages/SalesmanDashboard';
 import IndexPage from './pages/IndexPage';
 import DoctorForm from './pages/DoctorForm';
 import AgentForm from './pages/AgentForm';
@@ -13,6 +16,9 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/salesman-dashboard" element={<SalesmanDashboard />} />
           <Route path="/" element={<IndexPage />} />
           <Route path="/doctor" element={<DoctorForm />} />
           <Route path="/agent" element={<AgentForm />} />
