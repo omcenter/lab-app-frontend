a4db254e39b1a7841a1b09d3074929753d694ca5
omcenter <omdiagnosticcenterdelhi@gmail.com>
2025-06-20 11:30:14 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Got suggestions? 💡</p>
+      <a href="https://wa.me/918882447570" target="_blank" rel="noreferrer">📲 WhatsApp: 8882447570</a>
+    </div>
+  );
+};
+
+export default Footer;
